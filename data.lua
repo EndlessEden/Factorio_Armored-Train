cf51c15f77bbff7c83240c79a65985822427cf0d
panda <40077496+pandamiami80s@users.noreply.github.com>
2019-05-09 22:59:36 +0900

    Add files via upload

--- data.lua
+++ data.lua
@@ -2,32 +2,32 @@
 require("prototypes.armored-locomotive-mk1")
 
 
+
 --ARMORED PLATFORMS
-require("prototypes.armored-platform-mk1")
-
-
---ARMORED WAGONS
-require("prototypes.armored-wagon-mk1")
-
-
---OTHER
-require("prototypes.armored-platform-radar-mk1")
-
-
---ARMORED TURRET PLATFORMS
 require("prototypes.armored-platform-minigun-mk1")
 require("prototypes.armored-platform-rocket-mk1")
 
+--Other
+require("prototypes.armored-platform-mk1")
+require("prototypes.armored-platform-radar-mk1")
 
---ARMORED TURRET WAGONS
+require("prototypes.armored-wagon-mk1")
+
+
+
+--ARMORED WAGONS
 require("prototypes.armored-wagon-cannon-mk1")
 require("prototypes.armored-wagon-chaingun-mk1")
+
+
 
 
 --TURRET PROXY
 require("prototypes.platform-minigun-turret-mk1")
 require("prototypes.platform-rocket-turret-mk1")
 require("prototypes.wagon-cannon-turret-mk1")
+
+
 
 
 --TECH
